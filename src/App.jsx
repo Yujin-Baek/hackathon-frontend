fd646bcdd754b8e0a57451501a367adb432b7c9c
rachel4w2 <rachel4w2@naver.com>
2024-03-25 00:08:12 +0900

    fix: msw warning 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { useEffect } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  const createShortURL = async (url) => {
+    const response = await fetch("/short-links", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ url }),
+    });
+    const data = await response.json();
+    console.log(data.shortURL);
+    return data.shortURL;
+  };
+
+  useEffect(() => {
+    createShortURL(
+      "https://medium.com/@hgbaek1128/querydsl%EC%9D%84-%EC%9D%B4%EC%9A%A9%ED%95%B4%EC%84%9C-pageable%EB%A1%9C-sorting%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95-65c63ebaebd8"
+    );
+  }, []);
+
+  return <div className="App">App</div>;
 }
 
 export default App;
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,12 +1,16 @@
 import { http, HttpResponse } from "msw";
 
 export const handlers = [
-  http.post("/auth", () => {
+  http.post("/short-links", () => {
     // Note that you DON'T have to stringify the JSON!
     return HttpResponse.json({
-      user: {
-        id: "abc-123",
-        name: "John Maverick",
+      data: {
+        id: 1,
+        shortUrl: "url-shortener/bf406",
+        originUrl:
+          "https://medium.com/@hgbaek1128/querydsl%EC%9D%84-%EC%9D%B4%EC%9A%A9%ED%95%B4%EC%84%9C-pageable%EB%A1%9C-sorting%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95-65c63ebaebd8",
+        hash: "bf406",
+        createdAt: "2021-06-07T11:38:16+0000",
       },
     });
   }),
