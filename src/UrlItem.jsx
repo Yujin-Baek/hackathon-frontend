04950a28cf30f12194372ac6cc56262eee1e91a9
rachel4w2 <rachel4w2@naver.com>
2024-03-25 21:17:42 +0900

    feat: 복사 버튼 추가

--- src/UrlItem.jsx
+++ src/UrlItem.jsx
@@ -1,11 +1,22 @@
+import { FaRegCopy } from "react-icons/fa6";
+
 export default function UrlItem({ url, handleDelete }) {
+  const handleCopy = (shortUrl) => {
+    navigator.clipboard.writeText(shortUrl);
+    alert("Copied to clipboard!");
+  };
   return (
     <li>
       <div id="url-container">
         <p>longUrl</p>
         <a href="url.originUrl">{url.originUrl}</a>
         <p>shortUrl</p>
-        <a href="url.shortUrl">{url.shortUrl}</a>
+        <div>
+          <a href="url.shortUrl">{url.shortUrl}</a>
+          <button id="copy-button" onClick={() => handleCopy(url.shortUrl)}>
+            <FaRegCopy />
+          </button>
+        </div>
       </div>
       <button id="delete-button" onClick={() => handleDelete(url.id)}>
         x
@@ -13,3 +24,4 @@ export default function UrlItem({ url, handleDelete }) {
     </li>
   );
 }
+//url-shortener/bf400
